d66d755fba139e7a8df0298f3e0ba9671d367944
카룽 <qownsrms24@gmail.com>
2023-09-16 14:15:31 +0000

    기본 Program.cs 파일 생성

--- src/Program.cs
+++ src/Program.cs
@@ -3,12 +3,54 @@ using System.IO;
 
 namespace GoormIDE_test{
 	public class ConstValue{
+		public const int WinSizeX = 100;
+		public const int WinSizeY = 40;
+		
 		public const string Version = "0.0.1";
 	}
 	
+	public class Print{
+		public void Center(string Str, int Y){
+			int ByteSize = System.Text.Encoding.Default.GetByteCount(Str);
+			
+			Console.SetCursorPosition((ConstValue.WinSizeX - ByteSize)/2, Y);
+			
+			Console.Write(Str);
+		}
+		
+		public void Center(string Str, int X, int Y){
+			int ByteSize = System.Text.Encoding.Default.GetByteCount(Str);
+			
+			Console.SetCursorPosition( X + (ConstValue.WinSizeX - X)/2 - (ByteSize)/2, Y);
+			
+			Console.Write(Str);
+		}
+		
+		public void Left(string Str, int X, int Y){
+			Console.SetCursorPosition(X, Y);
+			
+			Console.Write(Str);
+		}
+		
+		public void Right(string Str, int X, int Y){
+			int ByteSize = System.Text.Encoding.Default.GetByteCount(Str);
+			
+			Console.SetCursorPosition(X - ByteSize, Y);
+			
+			Console.Write(Str);
+		}
+	}
+	
 	class Program{
+		
 		static void Main(){
-			Console.WriteLine("GoormIDE test version :"+ConstValue.Version);
+			Print Prt = new Print();
+
+			Console.Clear();
+			Console.SetWindowSize(ConstValue.WinSizeX, ConstValue.WinSizeY);
+			Console.CursorVisible = false;
+			
+			Prt.Center("Version : "+ConstValue.Version, 10);
 		}
 	}
 }

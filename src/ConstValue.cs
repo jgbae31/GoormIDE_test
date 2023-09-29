3f7f8b75307f2097c5fea5dcecc95ebd23cf262e
카룽 <qownsrms24@gmail.com>
2023-09-29 14:54:03 +0000

    dictionary를 이용해서 ConstValue 값 로드 완료

--- src/ConstValue.cs
+++ src/ConstValue.cs
@@ -1,19 +1,38 @@
 using System;
+using System.IO;
 using System.Collections.Generic;
 
 namespace Sarahel{
 	public static class ConstValue{
-		//private static Dictionary<string, string> GameName = new Dictionary<string, string>();
-		//private static Dictionary<string, string> Version = new Dictionary<string, string>();
-		//private static Dictionary<string, int> Delayms = Dictionary<string, int>();
-		private static Dictionary<string, int> WinSzie = Dictionary<string, int>();
+		private static Dictionary<string, string> GameName = new Dictionary<string, string>();
+		private static Dictionary<string, string> Version = new Dictionary<string, string>();
+		private static Dictionary<string, int> Delayms = new Dictionary<string, int>();
+		private static Dictionary<string, int> WinSize = new Dictionary<string, int>();
 		
 		public static void LoadSetFile(string File){
 			string Path = "/workspace/GoormIDE_test/setting/";
 			
-			string[] a = FileIO.ReadFile(Path+File);
+			StreamReader Sr = new StreamReader(Path+File);
 			
-			for(int i = 0; i < a.Length; i++) Console.WriteLine(a[i]);
+			while(!Sr.EndOfStream){
+				string Line = Sr.ReadLine();
+				string[] Data = Line.Split(',');
+				
+				switch(File){
+					case "GameName":
+						GameName.Add(Data[0], Data[1]);
+						break;
+					case "Version":
+						Version.Add(Data[0], Data[1]);
+						break;
+					case "Delayms":
+						Delayms.Add(Data[0], Convert.ToInt32(Data[1]));
+						break;
+					case "WinSize":
+						WinSize.Add(Data[0], Convert.ToInt32(Data[1]));
+						break;
+				}
+			}
 		}
 	}
 }

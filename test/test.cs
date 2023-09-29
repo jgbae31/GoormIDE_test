1477c7eab617d60db9560a68fb84ed28fd0b0730
카룽 <qownsrms24@gmail.com>
2023-09-29 14:53:38 +0000

    소비전력에 따른 배터리 가용 시간 계산기 작성

--- test/test.cs
+++ test/test.cs
@@ -4,14 +4,15 @@ using System.IO;
 class test{
 	
 	public static void Main(){
+		double ba, bv, uw, uv;
+		
 		string[] a = File.ReadAllLines("/workspace/GoormIDE_test/test/test");
 		
+		ba = Convert.ToDouble(a[0]);
+		bv = Convert.ToDouble(a[1]);
+		uw = Convert.ToDouble(a[2]);
+		uv = Convert.ToDouble(a[3]);
 		
-		Console.Clear();
-		
-		for(int i = 0; i < a.Length; i++){
-			Console.WriteLine(a[i]);
-		}
-		
+		Console.WriteLine(ba / ((uw / uv) * bv));
 	}
 }
